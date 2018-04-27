24509f8b7c318be597c34b672e19dbd111a02c4b
unknown <corth@luc.edu>
2018-04-27 01:23:25 -0500

    Added Try/Except clauses to handle errors reguarding the input of Reddit and Twitter secret keys. Program will now still run even if keys are absent.

--- project1/polls/reddit_interface.py
+++ project1/polls/reddit_interface.py
@@ -3,12 +3,18 @@ import os
 
 # https://praw.readthedocs.io/en/latest/getting_started/authentication.html#oauth
 
-reddit = praw.Reddit(client_id=os.environ['reddit_client_id'],
-                     client_secret= os.environ['reddit_secret_token'],
-                     redirect_uri='http://localhost:8080',
-                     user_agent='test script')
+try:
+    reddit = praw.Reddit(client_id=os.environ['reddit_client_id'],
+                         client_secret= os.environ['reddit_secret_token'],
+                         redirect_uri='http://localhost:8080',
+                         user_agent='test script')
+
+    reddit_api = reddit.subreddit("all")
+except:
+    print('Failed to import Reddit Keys')
+    reddit = None
+
 # print(reddit.auth.url(['identity'], '...', 'permanent'))
 
-reddit_api = reddit.subreddit("all")
 # for i in all.search("yellow car", limit=5) :
     # print(i.title)
