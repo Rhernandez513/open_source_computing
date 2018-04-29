f77bf95921cee8432307f25689f4f998b3a76adc
unknown <corth@luc.edu>
2018-04-29 04:01:11 -0500

    Changed how the secret keys are imported to the interface files.

--- project1/polls/flickr_interface.py
+++ project1/polls/flickr_interface.py
@@ -1,13 +1,20 @@
 from flickrapi import FlickrAPI
+from . import secret
 import os
 
 # https://github.com/alexis-mignon/python-flickr-api/tree/master/flickr_api
 
-try:
-    flickr = FlickrAPI(api_key = os.environ['flickr_api_key'],
-                       api_secret = os.environ['flickr_api_secret'],
-                       format='parsed-json')
-except:
-    print('Failed to import Flickr Keys')
-    flickr = None
+api_key = secret.keys['flickr_api_key']
+api_secret = secret.keys['flickr_api_secret']
+
+
+if api_key != '' and api_secret != '':
+    try:
+        flickr = FlickrAPI(api_key,
+                           api_secret,
+                           format='parsed-json')
+    except:
+        print('Failed to import Flickr Keys')
+else:
+    print('One or more Flickr Key values have not been entered in secret.py')
 
---
+++ project1/polls/secret.py
@@ -0,0 +1,13 @@
+keys = {
+    'flickr_api_key' : '',
+    'flickr_api_secret' : '',
+    
+    'twitter_consumer_key' : '',
+    'twitter_consumer_secret' : '',
+    'twitter_access_token' : '',
+    'twitter_access_token_secret' : '',
+    
+    'reddit_client_id' : '',
+    'reddit_secret_token' : '',
+
+}
